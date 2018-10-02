ca9300258d96ae90a93755771e032b1a543a8d3f
Connor Wesson <ccwesson@knights.ucf.edu>
2018-10-02 15:40:20 -0400

    list rows of data and denote headers

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -4,5 +4,7 @@ csvpath = os.path.join('X:/Users/cwess_000/Data_Bootcamp/UCFLM201809DATA2/03-Pyt
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     print(csvreader)
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
     for row in csvreader:
         print(row)
